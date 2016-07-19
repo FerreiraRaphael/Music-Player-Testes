afafb766d053d8d835e427d1cbb48ae3e1cbf15d
Raphael Ferreira <raphaelbsferreira@gmail.com>
2016-07-18 23:07:14 -0300

    merge de versões do trabalho e casa

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -6,18 +6,10 @@ import classes from './Player.scss'
 
 // Player component class
 class Player extends React.Component {
-<<<<<<< HEAD
 
-  render(){
-    // Dynamic class names with ClassNames
-    const playPauseClass =  this.props.playStatus ? 'fa fa-play' : 'fa fa-pause'
-    const { player, player__backward, player__forward, player__main, player__container,
-            player__info, player__cover, player__details ,progress, progress__bar } = classes
-=======
-  
   constructor(props) {
     super(props)
-    
+
   }
   componentDidMount(){
     let selector = `.${classes.progress__container}`
@@ -26,8 +18,8 @@ class Player extends React.Component {
       let progress__bar = document.querySelector(`.${classes.progress__bar}`)
       progress__bar.onmousemove = (e) => {
         let me = document.querySelector(`.${classes.progress__bar}`),
-            percent = e.offsetX / me.clientWidth,
-            progress = me.querySelector(`.${classes.progress}`)
+          percent = e.offsetX / me.clientWidth,
+          progress = me.querySelector(`.${classes.progress}`)
         percent = ((percent.toString().slice(0, 4) * 100) >= 99 ? 100 : percent.toString().slice(0, 4) * 100) + '%'
         progress.style.width = percent
       }
@@ -41,19 +33,20 @@ class Player extends React.Component {
     // Dynamic class names with ClassNames
     const playPauseClass =  this.props.playStatus ? 'fa fa-play' : 'fa fa-pause'
     const { player, player__backward, player__forward, player__main, playerContainer,
-            progress, progress__bar, progress__container } = classes  
->>>>>>> master
+      progress, progress__bar, progress__container } = classes
     // const playPauseClass = ClassNames({
     //   'fa fa-play': this.props.playStatus == 'PLAYING' ? false : true,
     //   'fa fa-pause': this.props.playStatus == 'PLAYING' ? true : false
     // });
-    
+
     // Return JSX
     return (
-      <div className={player__container}>
+      <div className={playerContainer}>
         <ul className={player}>
+          {/*Rewind Button*/}
           <li className={player__backward}>
             <div onClick={this.props.backward}><i className="fa fa-backward"></i></div>
+            {/*Play/Pause div*/}
             <div onClick={this.props.togglePlay}><i className={playPauseClass}></i></div>
             <div onClick={this.props.forward}><i className="fa fa-forward"></i></div>
           </li>
@@ -66,17 +59,9 @@ class Player extends React.Component {
             </div>
             <span>5:00</span>
           </li>
+          {/*Forward Button*/}
           <li className={player__forward}>
-            <div className={player__info}>
-              <div className={player__cover}></div>
-              <div className={player__details}>
-                <span></span>
-                <span></span>
-              </div>
-            </div>
             <div onClick={this.props.random}><i className="fa fa-random"></i></div>
-            <div onClick={this.props.random}><i className="fa fa-list-ul"></i></div>
-            <div onClick={this.props.random}><i className="fa fa-repeat"></i></div>
           </li>
         </ul>
       </div>
