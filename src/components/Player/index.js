7202b97473a1e753c0f1ef291e8accaa52dec592
Raphael Ferreira <raphaelbsferreira@gmail.com>
2016-08-15 13:20:33 -0300

    Nova posição da barra de progresso

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -6,35 +6,32 @@ import icones from './icones.scss'
 // Player component
 const Player = props => {
   "use strict";
-  const { icons, player, player__backward, player__forward, player__main, player__container,
-    player__timer, progress, progress__bar, progress__container, player__button, player__volume,
+  const { icons, player, player__backward, player__forward, player__container,
+    progress, progress__bar, progress__container, player__button, player__volume,
     player__volume__progress__container, player__volume__progress__bar, player__volume__progress,
     player__volume__progress__box, player__collapse, player__menu } = classes
 
   return (
   <div className={player__container}>
-    <div className={`${player__menu}`}>
+    {/*<div className={`${player__menu}`}>
 
-    </div>
+    </div>*/}
     <ul className={player}>
+      <div className={progress__container} onMouseUp={props.onMouseUpHandler_Music}>
+        <div className={progress__bar} onMouseDown={props.onMouseDownHandler_Music} onMouseUp={props.onMouseUpHandler_Music}>
+          <div className={progress}></div>
+        </div>
+      </div>
       <li className={player__backward}>
-        <div> <div></div><span>Nome Artista - Nome Musica</span></div>
+        <div></div><span>Nome Artista - Nome Musica</span>
+        <span>0:00&nbsp;&nbsp;/&nbsp;&nbsp;5:00</span>
+      </li>
+      <li className={player__forward}>
         <div className={`btn ${player__button}`} onClick={props.backward}><i className={`${icones.icon_backward} ${icons}`}/></div>
         <div className={`btn ${player__button}`} onClick={props.togglePlay}><i className={`${icones.icon_play} ${icons}`}/></div>
         <div className={`btn ${player__button}`} onClick={props.forward}><i className={`${icones.icon_forward} ${icons}`}/></div>
-      </li>
-      <li className={player__main}>
-        <span className={player__timer}>0:00</span>
-        <div className={progress__container} onMouseUp={props.onMouseUpHandler_Music}>
-          <div className={progress__bar} onMouseDown={props.onMouseDownHandler_Music} onMouseUp={props.onMouseUpHandler_Music}>
-            <div className={progress}></div>
-          </div>
-        </div>
-        <span className={player__timer}>5:00</span>
-      </li>
-      <li className={player__forward}>
-        <div className={`btn ${player__collapse}`} onClick={props.collapse}>collapse</div>
-        {/*<div className={`btn ${player__button} ${player__volume}`} onClick={props.random}
+        {/*<div className={`btn ${player__collapse}`} onClick={props.collapse}>collapse</div>*/}
+        <div className={`btn ${player__button} ${player__volume}`} onClick={props.random}
              onMouseEnter={props.showVolume}
              onMouseLeave={props.hideVolume}>
           <div className={`${player__volume__progress__container}`}>
@@ -47,9 +44,9 @@ const Player = props => {
             </div>
           </div>
           <i className={`${icones.icon_volume} ${icons}`}/></div>
-        <div className={`btn ${player__button}`} onClick={props.random}><i className={`${icones.icon_list} ${icons}`}/></div>
         <div className={`btn ${player__button}`} onClick={props.random}><i className={`${icones.icon_random} ${icons}`}/></div>
-        <div className={`btn ${player__button}`} onClick={props.random}><i className={`${icones.icon_infinity} ${icons}`}/></div>*/}
+        <div className={`btn ${player__button}`} onClick={props.random}><i className={`${icones.icon_infinity} ${icons}`}/></div>
+        <div className={`btn ${player__button}`} onClick={props.random}><i className={`${icones.icon_list} ${icons}`}/></div>
       </li>
     </ul>
   </div>
